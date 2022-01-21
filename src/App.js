df674c56566f52d33654e4e8f57e518dd1333312
keyur diwan <keyurdiwan009@gmail.com>
2022-01-21 18:28:13 +0530

    cart page and thank you page..!

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 import {Navbar} from './Components/Navbar';
 import {Home} from './Components/Home';
 import {Products} from './Components/Product'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { ProductDetails } from './Components/ProductDetails';
 import { Cart } from './Components/Cart';
 import { Login } from './Components/login';
+import Done  from './Components/Done';
 
 function App() {
+
+  // const location = useNavigate();
   return (
  <>
-      <Navbar/>
+      <Navbar />
+      {/* {location.pathname === '/done' ? null : <Navbar />} */}
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/products" element={<Products/>}></Route>
         <Route path="/products/:id" element={<ProductDetails />}></Route>
 
         <Route path="/cart/:id" element={<Cart />}></Route>
+        <Route path="/done" element={<Done />}>
+         {/* {this.state.isAlertOpen ? <alert /> : null} */}
+        </Route>
 
       </Routes>
     </>
